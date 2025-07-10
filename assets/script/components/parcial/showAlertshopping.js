69d6645c2cfce4eeb7a3de8d7508824fec95bbd5
Bruno <bd687353@gmail.com>
2025-07-10 17:32:50 -0300

    Retrabalhando no alerta dos item para cada interação

--- assets/script/components/parcial/showAlertshopping.js
+++
@@ -1,9 +0,0 @@
-function showAlertshopping() {
-  const alerta = document.getElementById("alerta-adicionado");
-  alerta.classList.add("mostrar");
-
-  setTimeout(() => {
-    alerta.classList.remove("mostrar");
-  }, 5000); // 5 segundos
-}
-export default showAlertshopping;
